0cc7fb1633dd42d619f5909d370fa856d48708e9
DESKTOP-4RHKGEA\user1 <19sy97@gmail.com>
2022-06-14 13:45:23 +0900

    COMMIT

--- src/java06_class/constructor/ConstructorEx_02.java
+++ src/java06_class/constructor/ConstructorEx_02.java
@@ -7,9 +7,40 @@ class Constructor_02 {
 	private int n1;
 	private int n2;
 	
+	public Constructor_02(int n1, int n2) {	//멤버필드 x, y에 값을 대입
+		
+		System.out.println("매개변수가 2개인 생성자");
+		
+		this.n1 = n1;
+		this.n2 = n2;
+		
+		
+	}
+	
+	public Constructor_02(int n1) {
+		this(n1, 0);	//this생성자 호출
+		
+		System.out.println("매개변수가 1개인 생성자");
+		
+	}
+	
 	
 	//생성자
+	public Constructor_02() {
+		//this 생성자 호출하기
+		this(0, 0);
+			
+		System.out.println("디폴트 생성자");
+		
+//		this.n1 = 0;
+//		this.n2 = 0;			//멤버필드랑 같은 코드
+		
+	}
 	
+	//** this 생성자 호출
+	//	-> 생성자에서 오버로딩된 다른 생성자를 호출하는 방법
+	//	-> 생성자코드의 재사용성을 높여준다
+	// 	-> 생성자코드에서 첫번째 코드로만 사용할 수 있다
 	
 	
 	
@@ -33,5 +64,11 @@ public class ConstructorEx_02 {
 		Constructor_02 cons01 = new Constructor_02();
 		cons01.out();
 		
+		System.out.println("----------------------");
+		
+		Constructor_02 cons02 = new Constructor_02(555, 666);
+		
 	}
 }
+
+
