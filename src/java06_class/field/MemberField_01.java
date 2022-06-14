53def2aa192626ae3d1232e8ca765b092fc9cdf7
DESKTOP-4RHKGEA\user1 <19sy97@gmail.com>
2022-06-14 14:51:18 +0900

    COMMIT

--- src/java06_class/field/MemberField_01.java
+++ src/java06_class/field/MemberField_01.java
@@ -24,6 +24,7 @@ public class MemberField_01 {
 	System.out.println("---- 클래스 변수 테스트 ----");
 	
 	MemberField_02 mf04 = new MemberField_02();		//변수가 선언할 시점이 코드가 실행되고 있을 때라 동적임
+	
 	MemberField_02 mf05 = new MemberField_02();
 	
 	System.out.println( mf04.city );
@@ -35,13 +36,12 @@ public class MemberField_01 {
 	System.out.println( mf04.city );
 	System.out.println( mf05.city );
 	
+	System.out.println(">> 클래스명으로 접근하기");
 	MemberField_02.city = "Incheon";	//클래스명에 멤버 필드 지정
 	System.out.println( MemberField_02.city );
 	
 	// **클래스 변수를 사용할 땐 객체 변수가 아닌 클래스명으로 참조해서 사용
 	
-	
-	
-	
+
 		}
 	}
