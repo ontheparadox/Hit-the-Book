ab3f7970eeffef4f62c5755e646ffccea93aeaf7
DESKTOP-4RHKGEA\user1 <19sy97@gmail.com>
2022-06-14 14:44:56 +0900

    COMMIT

--- src/java06_class/field/MemberField_01.java
+++ src/java06_class/field/MemberField_01.java
@@ -5,6 +5,8 @@ public class MemberField_01 {
 	//멤버 필드
 	private int num = 999;
 	private String name = "Alice";
+	
+	
 
 	public int getNum() {
 		return num;
@@ -17,4 +19,29 @@ public class MemberField_01 {
 	}
 	public void setName(String name) {
 		this.name = name;
-	}}
+		
+	System.out.println();
+	System.out.println("---- 클래스 변수 테스트 ----");
+	
+	MemberField_02 mf04 = new MemberField_02();		//변수가 선언할 시점이 코드가 실행되고 있을 때라 동적임
+	MemberField_02 mf05 = new MemberField_02();
+	
+	System.out.println( mf04.city );
+	System.out.println( mf05.city );
+	
+	mf04.city = "Busan";
+	
+	System.out.println(">> Busan 대입 후 ");
+	System.out.println( mf04.city );
+	System.out.println( mf05.city );
+	
+	MemberField_02.city = "Incheon";	//클래스명에 멤버 필드 지정
+	System.out.println( MemberField_02.city );
+	
+	// **클래스 변수를 사용할 땐 객체 변수가 아닌 클래스명으로 참조해서 사용
+	
+	
+	
+	
+		}
+	}
--- src/java06_class/field/MemberField_02.java
+++ src/java06_class/field/MemberField_02.java
@@ -7,7 +7,10 @@ public class MemberField_02 {
 	
 	//멤버 필드 -> 클래스 변수, static 변수, 정적 변수
 	// 움직일 수 없는 고정된 상태의 변수
-	private static String city = "Seoul";
+//	private static String city = "Seoul";
+	static String city = "Seoul";
+	// -> static 변수는 public 접근제한자를 적용하는 게 일반적이다
+
 	
 	
 }
